bcbc0515a0d48bc288bb6475f5ba2ed43d08b850
cacaudev <cacauabreu2013@gmail.com>
2020-01-20 22:09:03 -0300

    [feat] (Components): Add new component and services folder

--- lab_01/src/App.js
+++ lab_01/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './styles.css';
 import Header from './components/Header';
+import Main from './views/main';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
