5bfc2162c9e6f71e0bbe4f51bddc0933016e433f
cacaudev <cacauabreu2013@gmail.com>
2020-01-22 16:37:16 -0200

    [feat] (Views): Add new view

---
+++ lab_01/src/views/register/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        Welcome to register page
+      </div>
+    )
+  };
+};
+
+export default Register;
