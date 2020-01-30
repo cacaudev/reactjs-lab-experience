92b63a6d419454cfb63b13c84957fe83328ab828
cacaudev <cacauabreu2013@gmail.com>
2020-01-30 10:37:49 -0200

    [fix] (Style): Fix style on header

--- lab_01/src/components/Header/index.js
+++ lab_01/src/components/Header/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './styles.css';
 
-const Header = () => {
-  return (
-    <header id="main-header">Magic The Gathering Cards</header>
-  );
-}
+const Header = () => (
+  <header id="main-header">Magic The Gathering Cards</header>
+);
 
 export default Header;
