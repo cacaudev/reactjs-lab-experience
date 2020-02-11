1c77a0ac5026691da3d648069c184d3ebbbb01fb
cacaudev <cacauabreu2013@gmail.com>
2020-02-11 14:34:06 -0200

    [feat] (Styles): Add hover message in construction

--- lab_01/src/components/Header/index.js
+++ lab_01/src/components/Header/index.js
@@ -2,16 +2,38 @@ import React from 'react';
 import './styles.css';
 import { magicLogo } from '../../assets/img';
 
+const links = [
+  {
+    name: 'Profile',
+    isImplemented: true,
+  },
+  {
+    name: 'About',
+    isImplemented: false,
+  },
+];
+
 const Header = () => (
   <header id="main-header">
     <span className="logo">
-      <img src={magicLogo} alt="MagicTheGathering Logo" width="120px" height="40px" />
+      <img src={magicLogo} alt="MagicTheGathering Logo" width="130px" height="80px" />
     </span>
     <span className="title">
       Magic The Gathering Cards
     </span>
     <span className="links">
-      {/*User*/}
+      {links.map((item) => (
+        <span className="link-text">
+          <span>
+            {item.name}
+          </span>
+          {!item.isImplemented && (
+            <span className="not-implemented">
+              In Construction
+            </span>
+          )}
+        </span>
+      ))}
     </span>
   </header>
 );
